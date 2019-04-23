8e94cdb78f6a86546aa3ab79d1d27c339315e1f8
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-04-23 20:18:40 +0200

    resolve jest typing issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,14 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-
   let statusBarSpy, splashScreenSpy, platformReadySpy, platformSpy;
 
   beforeEach(async(() => {
-    statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
-    splashScreenSpy = jasmine.createSpyObj('SplashScreen', ['hide']);
+    statusBarSpy = jest.setMock('StatusBar', ['styleDefault']);
+    splashScreenSpy = jest.setMock('SplashScreen', ['hide']);
     platformReadySpy = Promise.resolve();
-    platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy });
+    platformSpy = jest.setMock('Platform', { ready: platformReadySpy });
 
     TestBed.configureTestingModule({
       declarations: [AppComponent],
@@ -23,8 +22,8 @@ describe('AppComponent', () => {
       providers: [
         { provide: StatusBar, useValue: statusBarSpy },
         { provide: SplashScreen, useValue: splashScreenSpy },
-        { provide: Platform, useValue: platformSpy },
-      ],
+        { provide: Platform, useValue: platformSpy }
+      ]
     }).compileComponents();
   }));
 
@@ -43,5 +42,4 @@ describe('AppComponent', () => {
   });
 
   // TODO: add more tests!
-
 });
