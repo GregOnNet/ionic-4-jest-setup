428858bde593bd7f004d825c3ec18f478be87971
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-05-03 11:00:12 +0200

    add two components that should be rendered in child outlet

---
+++ src/app/tabs2/app-one/app-one.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-one',
+  templateUrl: './app-one.component.html',
+  styleUrls: ['./app-one.component.scss'],
+})
+export class AppOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/tabs2/app-two/app-two.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-two',
+  templateUrl: './app-two.component.html',
+  styleUrls: ['./app-two.component.scss'],
+})
+export class AppTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
